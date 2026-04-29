bd547e63d3a77daf67c1619072a1898b00b7f6af
taylormaass <taylormaass04@gmail.com>
2026-04-28 23:25:40 -0700

    Delete streamlit_app.py

--- streamlit_app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-st.title("Alzheimer’s Prediction Dashboard")
-
-df = pd.read_csv("alzheimers_prediction_dataset.csv")
-
-st.write("Dataset Preview")
-st.dataframe(df)
